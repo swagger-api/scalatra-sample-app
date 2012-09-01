be5d97dce41c45d6f6d3df3ea8e33e84f59149bd
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-09-02 01:01:46 +0200

    also add bootstrap class

---
+++ src/main/scala/Scalatra.scala
@@ -0,0 +1,17 @@
+import com.wordnik.swagger.sample._
+import javax.servlet.ServletContext
+import org.scalatra.LifeCycle
+
+class Scalatra extends LifeCycle {
+  implicit val swagger = new PetstoreSwagger
+
+  override def init(context: ServletContext) {
+    try {
+      context mount (new PetServlet, "/pet")
+      context mount (new ResourcesApp, "/")
+    } catch {
+      case e: Throwable => e.printStackTrace()
+    }
+  }
+
+}
